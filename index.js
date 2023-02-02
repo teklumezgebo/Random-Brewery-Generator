1033c4c9a08ac0c0e01bb1f1713af54c9514670f
teklumezgebo <mezge002@umn.edu>
2023-02-02 17:41:55 -0600

    added goals to index.js in order to give myself structure for procject

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-fetch("https://api.openbrewerydb.org/breweries/search?query={search}&per_page=5")
-.then(function(response) {
-    console.log(response)
-    return response.json
-})
-.then(function(json) {
-     console.log(json)
-})
+// API link: "https://api.openbrewerydb.org/breweries/random?size=5"
+
+/* For array iteration I will use the .filter() method to extract elements relevant to the data I want to display to the user. */
+
+/* Event listener ideas:
+Button that allows for refresh of current page. ("click")
+Color change on mouse over, white -> blue. https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event ("mouseover")
+A message pops up from the brower to notify the user that the copied item was copied. https://developer.mozilla.org/en-US/docs/Web/API/Element/copy_event ("copy")
+*/ 
 
 
 
